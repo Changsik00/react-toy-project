9aec94680a0cfcf626f46aacd77316e5ad3495e1
changsik <lowmans00@gmail.com>
2024-07-14 23:58:50 +0900

    fix: ResponsiveLayout 외곽 사이즈가 전체 사이즈가 되도록 수정

--- src/components/common/ResponsiveLayout.tsx
+++ src/components/common/ResponsiveLayout.tsx
@@ -1,7 +1,7 @@
 const ResponsiveLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='flex min-h-screen items-center justify-center bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-white'>
-      <div className='w-full rounded-lg bg-white p-8 shadow-md sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl dark:bg-gray-800 dark:text-white'>
+    <div className='w-100vw h-100vh flex items-center justify-center bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-white'>
+      <div className='w-full max-w-full rounded-lg bg-white p-8 shadow-md sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl dark:bg-gray-800 dark:text-white'>
         {children}
       </div>
     </div>
