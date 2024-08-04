53e95380e09c7c06539087ea34e31ab155d211b5
changsik <lowmans00@gmail.com>
2024-08-04 18:04:41 +0900

    feat: 핸들러 코드 분리 및 중복 데이터 통합
    - 로그인 및 유저 정보 요청 핸들러를 개별 파일로 분리
    - 중복된 유저 데이터 공통 모듈로 분리
    - 공통 응답 생성 함수 createResponse 유틸리티 파일로 분리
    - 핸들러 파일 TypeScript로 전환

---
+++ src/mocks/handlers/handleFetchUser.ts
@@ -0,0 +1,22 @@
+// src/handlers/user/handleFetchUser.ts
+
+import { HttpResponse } from 'msw'
+import { users } from '../database/users'
+
+const createResponse = (data: object, status: number = 200) => {
+  return new HttpResponse(JSON.stringify(data), {
+    status,
+    headers: { 'Content-Type': 'application/json' },
+  })
+}
+
+export const handleFetchUser = ({ params }: { params: { userId: string | string[] } }) => {
+  const userId = Array.isArray(params.userId) ? params.userId[0] : params.userId
+  const user = users.find((user) => user.id === parseInt(userId, 10))
+
+  if (user) {
+    return createResponse(user)
+  }
+
+  return createResponse({ message: 'User not found' }, 404)
+}
---
+++ src/mocks/handlers/handleLogin.ts
@@ -0,0 +1,35 @@
+import { HttpResponse, delay } from 'msw'
+import { parseJSON } from '../../utils/parseJSON'
+import { z } from 'zod'
+
+import { users } from '../database/users'
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+})
+
+const createResponse = (data: object, status: number = 200) => {
+  return new HttpResponse(JSON.stringify(data), {
+    status,
+    headers: { 'Content-Type': 'application/json' },
+  })
+}
+
+export const handleLogin = async ({ request }: { request: Request }) => {
+  await delay(500)
+  try {
+    const { email, password } = await parseJSON(request, loginSchema)
+
+    const user = users.find((user) => user.email === email && user.password === password)
+
+    if (user) {
+      return createResponse(user)
+    }
+
+    return createResponse({ message: 'Invalid credentials' }, 401)
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred'
+    return createResponse({ message: errorMessage }, 400)
+  }
+}
