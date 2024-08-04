182027f4e4593a68addf7917ba108ec4d5bd3c01
changsik <lowmans00@gmail.com>
2024-08-04 21:32:05 +0900

    feat: login 페이지 이동시 다시 돌아갈수 있는 redirect 기능추가
    - useLocation & from: redirect 기능으로 이전 페이지 기록 및 전송

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,11 +1,16 @@
-import { Navigate, Outlet } from 'react-router-dom'
+import { Navigate, Outlet, useLocation } from 'react-router-dom'
 import { useAuth } from '../hooks/useAuth'
-
 const ProtectedRoute = () => {
   const { user } = useAuth()
-
+  const location = useLocation()
   if (!user) {
-    return <Navigate to='/login' replace />
+    const isDefaultRedirect = location.pathname === '/' || location.pathname === '/login'
+    if (isDefaultRedirect) {
+      return <Navigate to='/login' replace />
+    }
+
+    const redirect = location.pathname + location.search
+    return <Navigate to='/login' state={{ from: redirect }} replace />
   }
 
   return <Outlet />
